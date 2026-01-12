a663f2d83ff1dc423534fa2c0453f3e818a9abf1
Ryu Sato <ryu@weseek.co.jp>
2026-01-12 12:58:12 +0000

    loop

--- condition/src/main.rs
+++ condition/src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
-    let condition = true;
-    let number = if condition { 5 } else { 6 };
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
 
-    // numberの値は、{}です
-    println!("The value of number is: {}", number);
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {}", count);
 }
