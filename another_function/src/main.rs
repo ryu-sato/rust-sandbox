6152cd5f1569633484d15217caf5830645877d92
Ryu Sato <ryu@weseek.co.jp>
2026-01-12 09:42:03 +0000

    関数の引数

--- another_function/src/main.rs
+++ another_function/src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
-    println!("Hello, world!");
-
-    another_function();
+    another_function(5);
 }
 
-fn another_function() {
-    println!("Another function.");  // 別の関数
+fn another_function(x: i32) {
+    println!("The value of x is: {}", x);   // xの値は{}です
 }
