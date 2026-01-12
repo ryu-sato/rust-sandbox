cd3fa5ab53c9dde712015b27024a5718f25408a5
Ryu Sato <ryu@weseek.co.jp>
2026-01-12 12:52:05 +0000

    else if

--- condition/src/main.rs
+++ condition/src/main.rs
@@ -1,7 +1,17 @@
 fn main() {
-    let number = 3;
+    let number = 6;
 
-    if number != 0 {
-        println!("number was something other than zero");   // 数値は0以外の何かです
+    if number % 4 == 0 {
+        // 数値は4で割り切れます
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        // 数値は3で割り切れます
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        // 数値は2で割り切れます
+        println!("number is divisible by 2");
+    } else {
+        // 数値は4、3、2で割り切れません
+        println!("number is not divisible by 4, 3, or 2");
     }
 }
