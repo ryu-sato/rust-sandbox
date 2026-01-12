b058296d6e2a242183c5569b30627a30babfbb67
Ryu Sato <ryu@weseek.co.jp>
2026-01-12 12:51:38 +0000

    number was something other than zero

--- condition/src/main.rs
+++ condition/src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
     let number = 3;
 
-    if number < 5 {
-        println!("condition was true");       // 条件は真でした
-    } else {
-        println!("condition was false");      // 条件は偽でした
+    if number != 0 {
+        println!("number was something other than zero");   // 数値は0以外の何かです
     }
 }
