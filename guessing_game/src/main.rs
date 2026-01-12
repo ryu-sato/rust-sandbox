e4d719469787f90655a7796221b27f5849108bec
Ryu Sato <ryu@weseek.co.jp>
2026-01-12 08:34:57 +0000

    正解数値を表示しないようにした

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,8 +7,6 @@ fn main() {
 
     let secret_number = rand::rng().random_range(1..101);
 
-    println!("The secret number is: {}", secret_number);
-
     loop {
         println!("Please input your guess.");
 
