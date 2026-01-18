17e1a31d055cbec090035f5bef36b09e10d005e4
Ryu Sato <ryu@weseek.co.jp>
2026-01-18 16:19:43 +0000

    move ownership on return

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,16 +1,19 @@
 fn main() {
-    let s = String::from("hello");
-    takes_ownership(s);
+    let s1 = gives_ownership();
+    println!("{}", s1);
 
-    let x = 5;
-    makes_copy(x);
-    println!("{}", x);
+    let s2 = String::from("world");
+    println!("{}", s2);
+
+    let s3 = takes_and_gives_ownership(s2);
+    println!("{}", s3);
 }
 
-fn takes_ownership(some_string: String) {
-    println!("{}", some_string);
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+    some_string
 }
 
-fn makes_copy(some_integer: i32) {
-    println!("{}", some_integer);
+fn takes_and_gives_ownership(a_string: String) -> String {
+    a_string
 }
