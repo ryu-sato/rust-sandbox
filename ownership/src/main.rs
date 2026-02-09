0caf47c4bd8b32365a6c44eb8c8303e388fd9ab4
Ryu Sato <ryu@weseek.co.jp>
2026-02-09 17:00:49 +0000

    可変参照渡し

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
-    let s1 = String::from("hello");
+    let mut s1 = String::from("hello");
 
+    change(&mut s1);
     let len = calculate_length(&s1);
 
     println!("The length of '{}' is {}.", s1, len);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world")
 }
 
 fn calculate_length(s: &String) -> usize {
