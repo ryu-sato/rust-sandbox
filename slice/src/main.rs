fd11ce25bc0aead5c665896ccb163c628a0096ad
Ryu Sato <ryu@weseek.co.jp>
2026-02-11 13:45:04 +0000

    first_word関数を修正し、最初の単語の長さを返すように変更

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -9,21 +9,21 @@ fn main() {
         .read_line(&mut sentence)
         .expect("Failed to read line");
 
-    let first_word = first_word(&sentence);
+    let first_word_len = first_word(&sentence);
 
-    println!("You input: {}, first word: {}", sentence, first_word);
+    println!("You input: {}, first word: {}", sentence, &sentence[..first_word_len]);
 }
 
-fn first_word(s: &str) -> &str {
+fn first_word(s: &str) -> usize {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
             println!("First word ends at index: {}", i);
-            return &s[0..i];
+            return i;
         }
     }
 
     println!("The whole string is a single word.");
-    &s[..]
+    s.len()
 }
