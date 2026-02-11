2b8a164f617f49bd879cad2764a823c01e810ae4
Ryu Sato <ryu@weseek.co.jp>
2026-02-11 13:37:50 +0000

    ユーザー入力を受け取り、最初の単語を抽出する機能を追加

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please input some sentence.");
+
+    let mut sentence = String::new();
+
+    io::stdin()
+        .read_line(&mut sentence)
+        .expect("Failed to read line");
+
+    let first_word = first_word(&sentence);
+
+    println!("You input: {}, first word: {}", sentence, first_word);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            println!("First word ends at index: {}", i);
+            return &s[0..i];
+        }
+    }
+
+    println!("The whole string is a single word.");
+    &s[..]
 }
